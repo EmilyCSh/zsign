aa5bf0f20cbc405d7045554c6f1496bb9a920973
Emily <info@emy.sh>
2026-02-16 14:12:42 +0100

    Remove useless code
    Signed-off-by: Emily <info@emy.sh>

--- src/zsign_capi.cpp
+++ src/zsign_capi.cpp
@@ -7,10 +7,10 @@
 
 extern "C" {
 
-int zsign_sign_folder_to_ipa(const char* app_folder, const char* output_ipa, const char* prov_path, const char* key_path, char** out_bundle_id, char** out_bundle_ver)
+int zsign_sign_folder_to_ipa(const char* app_folder, const char* prov_path, const char* key_path)
 {
     try {
-        if (!app_folder || !output_ipa) {
+        if (!app_folder) {
             return -1;
         }
 
@@ -33,42 +33,6 @@ int zsign_sign_folder_to_ipa(const char* app_folder, const char* output_ipa, con
         if (!ok) {
             return -3;
         }
-
-        std::string infoPath = bundle.m_strAppFolder + "/Info.plist";
-
-        jvalue jvInfo;
-        if (!jvInfo.read_plist_from_file(infoPath.c_str())) {
-            return -4;
-        }
-
-        std::string bundle_id = jvInfo["CFBundleIdentifier"].as_cstr();
-        std::string bundle_ver = jvInfo["CFBundleVersion"].as_cstr();
-
-        size_t pos = bundle.m_strAppFolder.rfind("Payload");
-        if (std::string::npos == pos) {
-            if (!Zip::Archive(bundle.m_strAppFolder.c_str(), output_ipa, 9)) {
-                return -5;
-            }
-        } else {
-            std::string baseFolder = bundle.m_strAppFolder.substr(0, pos - 1);
-
-            if (!Zip::Archive(baseFolder.c_str(), output_ipa, 9)) {
-                return -5;
-            }
-        }
-
-        if (bundle_id.empty() || bundle_ver.empty()) {
-            return -6;
-        }
-
-        if (out_bundle_id) {
-            *out_bundle_id = strdup(bundle_id.c_str());
-        }
-
-        if (out_bundle_ver) {
-            *out_bundle_ver = strdup(bundle_ver.c_str());
-        }
-
         return 0;
     } catch(const std::runtime_error& re) {
         std::cerr << "Runtime error: " << re.what() << std::endl;
